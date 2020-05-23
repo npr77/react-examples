8329fc357c9e81c79c8e54dee2c6419601e5f167
npr77 <mail@nick-prosch.de>
2020-05-23 11:22:17 +0200

    parent child commnication wip

--- src/components/data.js
+++ src/components/data.js
@@ -1,11 +1,61 @@
 import React from 'react';
 
-export default class Date extends React.Component {
+class Parents extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.renderChildren = this.renderChildren.bind(this);
+        this.state = {
+            children: props.children,
+        };
+    }
+
+    renderChildren() {
+        return (
+            <div>TODO</div>
+        );
+    }
+
+    render() {
+        return (
+            <div className="border">
+                <h3>Parents</h3>
+                {this.renderChildren}
+            </div>
+        )
+    }
+}
+
+class Child extends React.Component {
+
+    render() {
+
+        const name = this.props.name;
+
+        return (
+            <div className="border">
+                <p>Child: {name}</p>
+            </div>
+        )
+    }
+}
+
+
+export default class Data extends React.Component {
 
     render() {
 
         return (
-            <div>Data example upcoming</div>
+
+            <div>
+                <h2>Data example</h2>
+                <Parents>
+                    <Child name="foo" />
+                    <Child name="bar" />
+                </Parents>
+                <Child name="solo" />
+
+            </div>
         );
     }
 }
